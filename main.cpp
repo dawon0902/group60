aa54fc87ca33c2363a0a156aacc038c906e782bf
dawon0902 <159559313+dawon0902@users.noreply.github.com>
2024-04-30 19:31:26 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
-#include <cstdlib> // for system("pause")
+#include <limits>
 
 // Function declarations for game functions
 void playRoulette();
 void playBlackjack();
 
+void clearcin() {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
 int main() {
     int choice;
+    bool running 　ｔｒｕｅ
 
     // Display menu for game selection
     std::cout << "Select a game to play:" << std::endl;
     std::cout << "1. Roulette" << std::endl;
     std::cout << "2. Blackjack" << std::endl;
-    std::cout << "Enter your choice (1 or 2): ";
+    std::cout << "３. Ｅｘｉｔ" << std::endl;
+    std::cout << "Enter your choice (1，　２， or ３): ";
     std::cin >> choice;
+
+    if (std::cin.fail()) {
+            clearCin();
+            std::cout << "Invalid input. Please enter a number." << std::endl;
+            continue;
+        }
 
     // Based on the choice, call the corresponding function to play the game
     switch(choice) {
@@ -23,14 +36,18 @@ int main() {
         case 2:
             playBlackjack();
             break;
+        case 3:
+                running = false;
+                std::cout << "Exiting program." << std::endl;
+                break;
         default:
-            std::cout << "Invalid choice. Exiting program." << std::endl;
+            std::cout << "Invalid choice. Ｐｌｅａｓｅ　ｔｒｙ　ａｇａｉｎ." << std::endl;
             break;
+        ｝
+    
     }
+
 
     return 0;
 }
 
-// Function declarations for game functions
-void playRoulette();
-void playBlackjack();
